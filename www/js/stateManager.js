fef333276077c30e78921f8a66f2bd7755fcda51
Williams Rincon <wwallass.0310@gmail.com>
2017-07-18 15:55:02 -0500

    added cordova

---
+++ www/js/stateManager.js
@@ -0,0 +1,33 @@
+var w = window.innerWidth * window.devicePixelRatio,
+    h = window.innerHeight * window.devicePixelRatio;
+
+/*
+For Fullscreen put this code:
+
+var w = window.innerWidth * window.devicePixelRatio,
+    h = window.innerHeight * window.devicePixelRatio;
+*/
+
+var game = new Phaser.Game(w, h, Phaser.AUTO, 'gameContainer');
+
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('menu', menuState);
+//game stages
+game.state.add('letter-a', letterA_State);
+game.state.add('letter-e', letterE_State);
+game.state.add('letter-i', letterI_State);
+game.state.add('letter-o', letterO_State);
+game.state.add('letter-u', letterU_State);
+//Results
+game.state.add('result-a', resultadoA_State);
+game.state.add('result-e', resultadoE_State);
+game.state.add('result-i', resultadoI_State);
+game.state.add('result-o', resultadoO_State);
+game.state.add('result-u', resultadoU_State);
+
+
+
+
+
+game.state.start('boot');
